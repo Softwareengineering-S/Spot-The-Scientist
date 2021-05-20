3701800f5bc5f7b5f82c2dea175cef27d5145a80
Softwareengineering-S <79382145+Softwareengineering-S@users.noreply.github.com>
2021-05-20 09:50:42 +0200

    Add files via upload

---
+++ SpotTheScientist/SpotTheScientist/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SpotTheScientist
+//
+//  Created by Monique Shaqiri on 08.05.21.
+//  Copyright © 2021 Monique Shaqiri. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+    
+      
+        
+        
+      
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
